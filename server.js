60a7dc1ddd727427a8cf09871f7e644fa3452666
Tlaquito1 <samuelibarra202@gmail.com>
2026-04-06 15:28:33 -0600

    Update database connection details in server.js

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const mysql = require('mysql2');
 
-// Configuración para XAMPP / WampServer (Local)
 const db = mysql.createConnection({
     host: 'brogxb0kkak6vedqd3wx-mysql.services.clever-cloud.com',
     user: 'ucgtkn8difo5dn8k',
-    password: '', // Por defecto en XAMPP es vacío
-    database: 'PuntoVentaBD', // Asegúrate de que este nombre coincida con tu DB en PHPMyAdmin
+    password: 'M1OPrcPFa8a0eENu10Bz', // <--- Asegúrate que esté completa
+    database: 'brogxb0kkak6vedqd3wx',
     port: 3306
 });
 
 db.connect(err => {
     if (err) {
-        console.error("❌ Error al conectar localmente:", err.message);
-        console.log("Asegúrate de que XAMPP (MySQL) esté encendido.");
+        console.error("❌ Error de conexión:", err.message);
     } else {
-        console.log("✅ Conexión exitosa a la base de datos local: PuntoVentaBD");
+        console.log("✅ Conectado a Clever Cloud desde Render");
     }
 });
 
